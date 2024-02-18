030911a9e8575643ab05cc9180141210e258e9b9
Yuvraj Patil <5191208+night-fury-rider@users.noreply.github.com>
2024-02-18 12:11:06 +0530

    Added Module resolver.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          $clubhouse: './src/clubhouse',
+          $components: './src/clubhouse/components',
+          $constants: './src/clubhouse/constants',
+          $hooks: './src/clubhouse/hooks',
+          $navigation: './src/clubhouse/navigation',
+          $network: './src/clubhouse/network',
+          $redux: './src/clubhouse/redux',
+          $dashboard: './src/dashboard',
+          $settings: './src/settings',
+          $tools: './src/tools',
+        },
+      },
+    ],
+  ],
 };
