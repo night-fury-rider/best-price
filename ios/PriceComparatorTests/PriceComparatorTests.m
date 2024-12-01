6063d98078519fc539e08535ca7e77e11dc326f2
night-fury-rider <5191208+night-fury-rider@users.noreply.github.com>
2024-12-01 07:34:31 +0530

    Removed references of Price Comparator

--- ios/PriceComparatorTests/PriceComparatorTests.m
+++ ios/PriceComparatorTests/PriceComparatorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PriceComparatorTests : XCTestCase
+@interface BestPriceTests : XCTestCase
 
 @end
 
-@implementation PriceComparatorTests
+@implementation BestPriceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
